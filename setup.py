3c52f548468ef21751f8aacdde929355d74ef66e
Vaishnavi Gambhir Rao <142235054+Vaishnavi-1509@users.noreply.github.com>
2023-09-04 14:48:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,23 @@
+from setuptools import setup, find_packages
+import codecs
+import os
 
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A Speech Recognition Attendance for MySQL Database Management'
+LONG_DESCRIPTION = 'This library is built to convert user voice commands into sql commands by catching keywords spoken by the user using Artificial Intelligence of speech recognition.'
+
+# Setting up
+setup(
+    name="MySQLvoice",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    install_requires=['SpeechRecognition', 'pyaudio', 'mysql-connector-python' ],
+    keywords=["mycursor","tablename1","tablename2","t1c1","t1c2","t1c3","t2c1","t2c2","t3c3"],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
